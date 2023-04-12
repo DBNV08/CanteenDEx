ce940179fb428bfe437b5af45166689448f088cb
Arslan <zdannikovarslan@gmail.com>
2023-04-12 11:11:46 +0500

    Добавлена переход между страницами и вывод данных из базы данных

--- Pages/ViewPage.xaml.cs
+++ Pages/ViewPage.xaml.cs
@@ -20,9 +20,44 @@ namespace CanteenDEx.Pages
     /// </summary>
     public partial class ViewPage : Page
     {
+        CanteenDExEntities db;
+        Products ProductsTable;
+        public List<Products> ProductList;
+        int CurrentPage = 1;
+        int End = 14;
+        int Start = 0;
         public ViewPage()
         {
             InitializeComponent();
+            db = CanteenDExEntities.GetContext();
+            ProductsTable = new Products();
+            ProductList = db.Products.ToList();
+            for (int i = 1; i < 15; i++)
+                Lb.Items.Add(ProductList[i]);
+            NumOfPage.Content = CurrentPage.ToString();
+
+
+        }
+
+        private void PrevPage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (CurrentPage == 1) return;
+            CurrentPage--; Start -= 15; End = Start + 15;
+            Lb.Items.Clear();
+            for (int i = Start; i < End; i++)
+                Lb.Items.Add(ProductList[i]);
+            NumOfPage.Content = CurrentPage.ToString();
+        }
+
+        private void NextPage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Start + 15 > ProductList.Count) return;
+            CurrentPage++; Start += 15; End = Start + 15;
+            Lb.Items.Clear();
+            if (ProductList.Count - 15 < End) End = ProductList.Count;
+            for (int i = Start; i < End; i++)
+                Lb.Items.Add(ProductList[i]);
+            NumOfPage.Content = CurrentPage.ToString();
         }
     }
 }
